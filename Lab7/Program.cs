4e7c2ed7c6dbbd26a888d44e7bc5b72b0c697df8
Roman Akhmetov <romanbackof@gmail.com>
2023-10-11 23:20:48 +0300

    lab7 done

---
+++ Lab7/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// See https://aka.ms/new-console-template for more information
+
+using Lab7Lib;
+Mutex mutexObj = new();
+int pos = 7;
+var sol = new Integral();
+sol.ShowResult += (ts, ans) =>
+{
+    Console.SetCursorPosition(0, pos);
+    Console.WriteLine($"Thread {Thread.CurrentThread.Name}: completed with result: {ans}, by {ts}");
+    ++pos;
+};
+sol.ShowProgress += pr =>
+{
+    mutexObj.WaitOne();
+    Console.SetCursorPosition(0, Convert.ToInt32(Thread.CurrentThread.Name) - 1);
+    Console.Write($"Thread {Thread.CurrentThread.Name}:[");
+    for (int i = 0; i < (pr * 20 / 100); ++i)
+    {
+        Console.Write("=");
+    }
+
+    Console.Write(">");
+    for (int i = 0; i < 20 - (pr * 20 / 100); ++i)
+    {
+        Console.Write(" ");
+    }
+
+    Console.Write($"] {pr}%");
+    Console.WriteLine();
+    mutexObj.ReleaseMutex();
+
+};
+double l = 0.1;
+double r = 0.5;
+double dx = 0.00001;
+var thr = new Thread(( ) => sol.Solve(l, r, dx));
+thr.Name = "1";
+var thr2 = new Thread(( ) => sol.Solve(l, r, dx));
+thr2.Name = "2";
+var thr3 = new Thread(( ) => sol.Solve(l, r, dx));
+thr3.Name = "3";
+var thr4 = new Thread(( ) => sol.Solve(l, r, dx));
+thr4.Name = "4";
+var thr5 = new Thread(( ) => sol.Solve(l, r, dx));
+thr5.Name = "5";
+thr.Priority = ThreadPriority.Highest;
+thr2.Priority = ThreadPriority.Lowest;
+thr.Start();
+thr2.Start();
+thr3.Start();
+thr4.Start();
+thr5.Start();
---
+++ Lab7Lib/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace Lab7Lib;
+
+public class Integral
+{
+    public delegate void Result(TimeSpan ts, double ans);
+    public event Result ShowResult;
+
+    public delegate void Progress(int progress);
+
+    public event Progress ShowProgress; 
+    private Semaphore semaphore = new(2, 2);
+
+    public void Solve(double  lB = 0, double rB = 1, double d = 0.00000001)
+    {
+        
+        Stopwatch sp = new();
+        semaphore.WaitOne();
+        sp.Start();
+        double lBorder = (double)lB;
+        double rBorder = (double)rB;
+        double dx = (double)d;
+        
+        if (lBorder < 0 || lBorder > 1 || rBorder < 0 || rBorder > 1)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        if (lBorder > rBorder)
+        {
+            (lBorder, rBorder) = (rBorder, lBorder);
+        }
+
+        double sum = 0;
+        for (double i = lBorder; i <= rBorder; i += dx)
+        {
+            sum += dx * Math.Sin(i);
+            for (int j = 0; j < 100; ++j)
+            {
+                int b = 13 + j;
+            }
+            ShowProgress?.Invoke((int)( (i - lBorder) / (rBorder - lBorder) * 100));
+        }
+        sp.Stop();
+        ShowResult?.Invoke(sp.Elapsed, sum);
+        semaphore.Release();
+        //Console.WriteLine($"ended, answer : {sum}, time = {sp.Elapsed}");
+    }
+}
